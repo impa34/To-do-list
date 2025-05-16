92a403d977b0534f7cc7966964092930cd999d04
Bogdan Chayka <bogdanchaykafutur@gmail.com>
2025-05-16 10:34:49 +0200

    Added new styles, reordered CSS file and fixed some bugs

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ let tasks = document.getElementById("tasks");
 let prio = document.getElementById("prio-select");
 let deadline = document.getElementById("deadline");
 const button = document.getElementById("button");
+const form = document.getElementById("entry");
+
+
 // Added flatpickr for date management
 let flatpickrInstance;
 
@@ -15,11 +18,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
 // Function to add the new task into the html
 button.addEventListener("click", function () {
     // Added a task container to manage checkboxes and tasks simultaneoulsy
     let container = document.createElement("div");
-    container.classList.add("task-container");
+    container.classList.add("task-container", "task-enter");
 
     let checkBox = document.createElement("input");
     checkBox.type = "checkbox";
@@ -31,12 +35,11 @@ button.addEventListener("click", function () {
     // Setting a deadline
     let deadlineSpan = document.createElement("span");
     if (flatpickrInstance.selectedDates.length > 0) {
-        deadlineSpan.innerText = `${flatpickrInstance.formatDate(flatpickrInstance.selectedDates[0], "d/m/Y")})`
+        deadlineSpan.innerText = ` ${flatpickrInstance.formatDate(flatpickrInstance.selectedDates[0], "d/m/Y")}`
     } else {
         deadlineSpan.innerText = " (No deadline)";
     }
      
-    
     // Creating a span to store the value of the select form 
     let prioritySpan = document.createElement("span");
     prioritySpan.classList.add("priority-span");
@@ -47,8 +50,10 @@ button.addEventListener("click", function () {
         prioritySpan.classList.add("priority-high");
     } else if (prio.value === "Medium") {
         prioritySpan.classList.add("priority-medium");
-    } else {
+    } else if (prio.value === "Low") {
         prioritySpan.classList.add("priority-low");
+    } else {
+        return alert ("Must provide priority of the task");
     }
     // Validations
     newTask.innerText = listener.value
@@ -56,20 +61,27 @@ button.addEventListener("click", function () {
         return alert("Must set a name");
     }
     // Appending the tasks to the list
-    newTask.appendChild(prioritySpan);
     newTask.appendChild(deadlineSpan);
+    newTask.appendChild(prioritySpan);
     container.appendChild(checkBox);
     container.appendChild(newTask);
     tasks.appendChild(container);
+    
 
     // As checkbox is clicked text disappears
     checkBox.addEventListener("click", function() {
-        tasks.removeChild(container);
+        container.classList.remove("task-enter");
+        container.classList.add("task-exit");
+
+        setTimeout(() => {container.remove();
+        }, 400);
     })
+
+
     // Clear values after adding
     listener.value = "";
     flatpickrInstance.clear();
-    prio.value = "Priority";
+    prio.value = "";
 })
 
-document.body.append(button);
+form.append(button);
